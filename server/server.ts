b7375d41df133cebba3e78c36a90889222c51ccb
Aaron Schmidt <aaron.schmidt@thm.mni.de>
2023-07-06 11:49:42 +0200

    Imports eingebunden(express, mysql, chart.js, crypto) Port definiert(8080) und angebunden

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,18 @@
 import * as express from "express";
 import * as session from "express-session";
+import * as mysql from "mysql";
 import * as crypto from "crypto";
 //Install Displayable Chart option
 import Chart from 'chart.js';
+
+const PORT: number = 8080;
+
+const app: express.Express = express();
+const connection: mysql.Connection = mysql.createConnection({
+    database: "team-01",
+    host: "localhost",
+    user: "viewer"
+});
+
+
+
