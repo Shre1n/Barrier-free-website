6993fdba7514c18f76d499e5ac7182c8dec58de7
robin <robinhahn99@outlook.de>
2023-07-09 14:18:52 +0200

    Nutzeranlegen implementiert.

--- client/script.ts
+++ client/script.ts
@@ -2,14 +2,14 @@
 
 let modalFensterUser: bootstrap.Modal;
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("registrieren").addEventListener("submit", (event: Event) => addUser(event));
-
     modalFensterUser = new bootstrap.Modal(document.getElementById("ModalUser"));
 
     document.getElementById("registrieren").addEventListener("click",()=>{
        modalFensterUser.show();
-       document.getElementById("modalForm").addEventListener("submit", (event:Event) => addUser(event));
     });
+    document.getElementById("modalForm").addEventListener("submit", (event:Event) => addUser(event));
+
+
 });
 
 function addUser(event: Event): void {
@@ -21,8 +21,8 @@ function addUser(event: Event): void {
     const vorname: String = (document.getElementById("vorname") as HTMLInputElement).value;
     const nachname: String = (document.getElementById("nachname") as HTMLInputElement).value;
     const email: String = (document.getElementById("email") as HTMLInputElement).value;
-    const password: String = (document.getElementById("password") as HTMLInputElement).value;
-    const postleitzahl: String = (document.getElementById("PLZ") as HTMLInputElement).value;
+    const passwort: String = (document.getElementById("passwort") as HTMLInputElement).value;
+    const postleitzahl: String = (document.getElementById("postleitzahl") as HTMLInputElement).value;
     const ort: String = (document.getElementById("ort") as HTMLInputElement).value;
     const strasse: String = (document.getElementById("strasse") as HTMLInputElement).value;
     const hnr: String = (document.getElementById("hausnummer") as HTMLInputElement).value;
@@ -34,21 +34,20 @@ function addUser(event: Event): void {
         vorname: vorname,
         nachname: nachname,
         email: email,
-        password: password,
+        passwort: passwort,
         postleitzahl:postleitzahl,
         ort:ort,
         strasse:strasse,
         hnr: hnr,
         telefonnummer:telefonnummer
-
     }).then((res: AxiosResponse) => {
         console.log(res);
         //reset der Form zum Eintragen
         form.reset();
-        document.getElementById("signUpError").innerText = "";
+        document.getElementById("registrierenError").innerText = "";
     }).catch((reason: AxiosError) => {
         if (reason.response.status == 500) {
-            document.getElementById("signUpError").innerText = "Dieser Nutzer existiert bereits.";
+            document.getElementById("registrierenError").innerText = "Diese Email ist bereits vergeben.";
         }
         //Error Ausgabe in Console
         console.log(reason);
