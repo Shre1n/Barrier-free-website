c76dc914416397c5b5964badb84b24c6fd5b8fd3
Jonas Thiele <jonas.paul.thiele@mni.thm.de>
2023-07-11 14:24:46 +0200

    Registrierung muss gefixt werden

--- client/script.ts
+++ client/script.ts
@@ -1,34 +1,32 @@
 //import axios, {AxiosError, AxiosResponse} from "axios;
-
 let modalFensterUser: bootstrap.Modal;
-let modalFensterUserreg: bootstrap.Modal;
+let modalFensterUserLogin: bootstrap.Modal;
 document.addEventListener("DOMContentLoaded", () => {
     modalFensterUser = new bootstrap.Modal(document.getElementById("ModalUser"));
-    modalFensterUserreg = new bootstrap.Modal(document.querySelector("#ModalUserreg"));
+    modalFensterUserLogin = new bootstrap.Modal(document.getElementById("ModalUserLogin"));
     const registrieren = document.querySelector("#registrieren");
     const signupform = document.querySelector("#signupform");
-    const loginform= document.querySelector("#loginform");
+    const loginform = document.querySelector("#loginform");
     if (registrieren){
         registrieren.addEventListener("click", () =>{
+            modalFensterUserLogin.show();
+        });
+    }
+    if (signupform) {
+        signupform.addEventListener("click", () => {
+            modalFensterUserLogin.hide();
             modalFensterUser.show();
         });
     }
-    if(signupform){
-        signupform.addEventListener("click",()=>{
-            modalFensterUserreg.show();
-            modalFensterUser.hide();
-    });
-        if(loginform){
-            loginform.addEventListener("click",()=>{
-                modalFensterUser.show();
-                modalFensterUserreg.hide();
+    if (loginform) {
+            loginform.addEventListener("click", () => {
+                modalFensterUser.hide();
+                modalFensterUserLogin.show();
             })
         }
-}
     console.log(document.getElementById("modalForm"));
     document.getElementById("modalForm").addEventListener("submit", addUser);
 });
-
 
 function addUser(event: Event): void {
     event.preventDefault();
@@ -39,27 +37,60 @@ function addUser(event: Event): void {
     const vorname: String = (document.getElementById("vorname") as HTMLInputElement).value;
     const nachname: String = (document.getElementById("nachname") as HTMLInputElement).value;
     const email: String = (document.getElementById("email") as HTMLInputElement).value;
-    const passwort: String = (document.getElementById("passwort") as HTMLInputElement).value;
+    const telefonnummer: String = (document.getElementById("telefonnummer") as HTMLInputElement).value;
     const postleitzahl: String = (document.getElementById("postleitzahl") as HTMLInputElement).value;
     const ort: String = (document.getElementById("ort") as HTMLInputElement).value;
     const strasse: String = (document.getElementById("strasse") as HTMLInputElement).value;
     const hnr: String = (document.getElementById("hausnummer") as HTMLInputElement).value;
-    const telefonnummer: String = (document.getElementById("telefonnummer") as HTMLInputElement).value;
+    const passwort: String = (document.getElementById("passwort") as HTMLInputElement).value;
 
-    //routen aufruf welcher an den Server uebermittelt wird
-    //Axios dient als Middleware
+    //const passwordcheck: String = (document.getElementById("passwortcheck") as HTMLInputElement).value;
+
+/*
+    if (passwort == passwordcheck) {
+        //routen aufruf welcher an den Server uebermittelt wird
+        //Axios dient als Middleware
+        axios.post("/user", {
+            //JSON Body
+            anrede: anrede,
+            vorname: vorname,
+            nachname: nachname,
+            email: email,
+            passwort: passwort,
+            postleitzahl: postleitzahl,
+            ort: ort,
+            strasse: strasse,
+            hnr: hnr,
+            telefonnummer: telefonnummer
+        }).then((res: AxiosResponse) => {
+            console.log(res);
+            //reset der Form zum Eintragen
+            form.reset();
+            document.getElementById("registrierenError").innerText = "";
+            modalFensterUser.hide();
+        }).catch((reason: AxiosError) => {
+            if (reason.response.status == 400) {
+                document.getElementById("registrierenError").innerText = "Diese Email ist bereits vergeben.";
+            }
+            //Error Ausgabe in Console
+            console.log(reason);
+        });
+    } else if (passwort != passwordcheck) {
+        document.getElementById("registrierenError").innerText = "Password stimmt nicht überein";
+    }
+    */
     axios.post("/user", {
         //JSON Body
-        anrede:anrede,
+        anrede: anrede,
         vorname: vorname,
         nachname: nachname,
         email: email,
         passwort: passwort,
-        postleitzahl:postleitzahl,
-        ort:ort,
-        strasse:strasse,
+        postleitzahl: postleitzahl,
+        ort: ort,
+        strasse: strasse,
         hnr: hnr,
-        telefonnummer:telefonnummer
+        telefonnummer: telefonnummer
     }).then((res: AxiosResponse) => {
         console.log(res);
         //reset der Form zum Eintragen
@@ -74,5 +105,4 @@ function addUser(event: Event): void {
         console.log(reason);
     });
 }
-
 
