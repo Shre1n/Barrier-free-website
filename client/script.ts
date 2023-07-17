0da42364333f9e2239efcd6bf611116c06ccae7e
Jonas Thiele <jonas.paul.thiele@mni.thm.de>
2023-07-17 10:28:46 +0200

    Gesamtwert: als text hinzugefügt

--- client/script.ts
+++ client/script.ts
@@ -663,9 +663,9 @@ function warenkorbRender() {
     <div class="modal-footer">
       <div class="container">
         <div class="row">
-          <div class="col-10"></div>
-          <div class="col-2 text-end" id="summe">
-            ${endpreis.toFixed(2)} €
+          <div class="col-5"></div>
+          <div class="col-7 text-end" id="summe">
+          Gesamtwert: ${endpreis.toFixed(2)} €
           </div>
         </div>
         <div class="row">
